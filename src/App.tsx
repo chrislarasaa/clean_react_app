903a332ba7b39dffdd0a3e0ad9bb418db22331bb
Christian Lara <chris.larasaa@gmail.com>
2023-04-30 20:12:01 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h2" component="h1">
+        Hola Mundo
+      </Typography>
+    </>
+  );
+}
+
+export default App;
